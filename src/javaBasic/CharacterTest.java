3bede27fe9f7bfe48f9eb914396650830f5d1b0c
chuck47286 <chuck472861399@163.com>
2020-02-02 21:07:17 +0000

    2020-02-02 coding from the laptop.

--- src/javaBasic/CharacterTest.java
+++ src/javaBasic/CharacterTest.java
@@ -1,10 +1,28 @@
 package javaBasic;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class CharacterTest {
     public static void main(String[] args) {
-        String s = "//\\--ABCabc";
+        /*String s = "//\\--ABCabc";
         for (char c : s.toCharArray()) {
             System.out.print((int)c + " ");
         }
+        char c = 'a';
+        Character character = Character.valueOf(c);*/
+
+        Character[] chs = {'d', 'c','e', 'b', 'a'};
+        Arrays.sort(chs);
+        System.out.println(Arrays.toString(chs));
+
+        Arrays.sort(chs, new Comparator<Character>(){
+
+            @Override
+            public int compare(Character o1, Character o2) {
+                return o2 - o1;
+            }
+        });
+        System.out.println(Arrays.toString(chs));
     }
 }
